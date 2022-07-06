cb968824d84676d99ca720cd5e2ff650a2b37e90
Cole Ferguson <fergusoncole98@outlook.com>
2022-07-06 14:12:20 -0500

    Initial Commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using OperatorExercise;
+Console.WriteLine("Please input two numbers");
+int input1 = Convert.ToInt32(Console.ReadLine());
+int input2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The sum is equal to: "+ Simple_Calculator.Sum(input1,input2));
+Console.WriteLine("The subtraction is equal to: "+ Simple_Calculator.Subtract(input1, input2));
+Console.WriteLine("The multiplation is equal to: "+ Simple_Calculator.Multiply(input1, input2));
+Console.WriteLine(Simple_Calculator.Divide(input1, input2)); //returning string instead
+Console.WriteLine("The modulus is qual to: " + Simple_Calculator.Remainder(input1, input2));
+
+
+Console.WriteLine("What is the radius of your circle?");
+double radius = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("The area of your circle is: " + Simple_Calculator.Area(radius));
+
---
+++ OperatorExercise/SimpleCalc.cs
@@ -0,0 +1,37 @@
+﻿
+namespace OperatorExercise
+    
+{
+    internal class Simple_Calculator
+    {
+        public static int Sum(int x, int y)
+        { return x + y; }
+       public static int Subtract(int x, int y)
+        { return x - y; }
+
+        public static int Multiply(int x, int y)
+        
+         { return x * y; }
+        
+        public static string Divide(int x, int y)
+            
+        {
+            int quotient = x / y;
+            int remainder = x % y;
+            return $"{x}/{y} is {quotient} remainder {remainder}";
+
+        
+        }
+
+        public static int Remainder(int x, int y)
+        { return x % y; }
+
+        public static double Area(double x)
+        {
+            return Math.PI * Math.Pow(x, 2);
+        }
+    }
+ 
+
+}
+
